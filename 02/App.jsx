6cbbc6387b30e2725c98e5e7206b91a6b77bcc18
Toshihiro Akabori <toshihiro.akabori@yahoo.com>
2019-09-10 15:31:55 -0700

    Implement version 2

--- 02/App.jsx
+++ 02/App.jsx
@@ -6,15 +6,22 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      navbarDisplay: false,
+    };
+    this.toggleNavBar = this.toggleNavBar.bind(this);
+  }
 
-    }
+  toggleNavBar() {
+    this.setState({
+      navbarDisplay: !this.state.navbarDisplay,
+    });
   }
 
   render() {
     return (
       <>
-        <div className="hamburger-icon">&#9776;</div>
-        <Navbar />
+        <div className="hamburger-icon" onClick={this.toggleNavBar}>&#9776;</div>
+        <Navbar navbarDisplay={this.state.navbarDisplay} />
       </>
     );
   }
--- 02/Navbar.jsx
+++ 02/Navbar.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
-const Navbar = () => {
-  return (
-    <span className="navbar">
-      <div className="home">Home</div>
-      <div className="about">About</div>
-      <div className="contact">Contact</div>
-    </span>
+const Navbar = (props) => {
+  if (props.navbarDisplay) {
+    return (
+      <span className="navbar">
+        <div className="home">Home</div>
+        <div className="about">About</div>
+        <div className="contact">Contact</div>
+      </span>
+    );
+  }
+  else return (
+    <span className="navbar-hidden">
+        <div className="home">Home</div>
+        <div className="about">About</div>
+        <div className="contact">Contact</div>
+      </span>
   );
 }
 
